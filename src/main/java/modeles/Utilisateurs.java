62d3ffc685ff7533ca1600465d9b4fd6bb2074b2
Miora Ralambotiana <miora.ralambotiana@phelma.grenoble-inp.fr>
2016-03-31 10:12:17 +0200

    Utilisateurs, Competences, Coordonnes

--- src/main/java/modeles/Utilisateurs.java
+++ src/main/java/modeles/Utilisateurs.java
@@ -5,10 +5,42 @@
  */
 package modeles;
 
+import java.util.ArrayList;
+import java.util.Date;
+import modeles.outils.Competences;
+import modeles.outils.Coordonnees;
+
 /**
  *
  * @author ralambom
+ * @date 31/03/2016
  */
 public class Utilisateurs {
+    String email;
+    String mdp;
+    String nom;
+    String prénom;
+    Integer genre;
+    Date date;
+    Coordonnees coordonnees;
+    ArrayList<Competences> competences;
+    
+    public Utilisateurs(String email, String mdp, String nom, String prénom, Integer genre, Date date, Coordonnees coordonnees) {
+        this.email = email;
+        this.mdp = mdp;
+        this.nom = nom;
+        this.prénom = prénom;
+        this.genre = genre;
+        this.date = date;
+        this.coordonnees = coordonnees;
+        this.competences = new ArrayList<Competences>();
+    }
+    
+    // on efface et recopie tout à chaque modification
+    // solution plus économique ?
+    public void competencesAJour (ArrayList<Competences> competences) {
+        this.competences.clear();
+        this.competences.addAll(competences);
+    }
     
 }
---
+++ src/main/java/modeles/outils/Coordonnees.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package modeles.outils;
+
+/**
+ *
+ * @author ralambom
+ */
+public class Coordonnees {
+    Integer latitude;
+    Integer longitude;
+    
+    public Coordonnees (int latitude, Integer longitude) {
+        this.latitude = latitude;
+        this.longitude = longitude;
+    }
+    
+    //ajouter fonction convertissant adresse en coordonnées
+}
