bd220ed3cd7ee6d3825cc37a91d4335e11c4ba8a
Ralambom <miora.ralambotiana@gmail.com>
2016-04-09 17:11:22 +0200

    fonction ajouterCompetence à une tache (tacheAtomDAO)

--- src/main/java/dao/TacheAtomDAO.java
+++ src/main/java/dao/TacheAtomDAO.java
@@ -5,10 +5,36 @@
  */
 package dao;
 
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import javax.sql.DataSource;
+
 /**
  *
  * @author cl
  */
-public class TacheAtomDAO {
+public class TacheAtomDAO extends AbstractDataBaseDAO{
+
+    public TacheAtomDAO(DataSource ds) {
+        super(ds);
+    }
+    
+    public void ajouterCompetence(int id, String emailC, String emailEx, String competence) throws DAOException {
+        ResultSet rs = null;
+        String requeteSQL = "";
+        Connection conn = null;
+        try {
+            conn = getConnection();
+            Statement st = conn.createStatement();
+            requeteSQL = "INSERT INTO CompetencesTaches VALUES (" + id + ", \'"+ emailC + ", \'"+ emailEx + "\',\'" + competence + "\')";
+            st.executeUpdate(requeteSQL);
+        } catch (SQLException e) {
+            throw new DAOException("Erreur BD " + e.getMessage(), e);
+        } finally {
+            closeConnection(conn);
+        }
+    }
     
 }
