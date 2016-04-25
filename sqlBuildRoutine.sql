c98fff4328a93bc33798865d2152e6ca8655541a
Benoît <benoit.peynet@phelma.grenoble-inp.fr>
2016-04-25 16:27:40 +0200

    Adaptation de la contrainte sur l'évaluation suite au changement de celle dans la table Evaluations

--- sqlBuildRoutine.sql
+++ sqlBuildRoutine.sql
@@ -11,7 +11,7 @@ CREATE TABLE Utilisateurs (
     evaluation float,
     CHECK (email like '%@%.%'),
     CHECK (genre = 1 OR genre = 2),
-    CHECK (evaluation >= -1 AND evaluation <= 5)/*,
+    CHECK (evaluation >= -1 AND evaluation <= 10)/*,
     CHECK (latitude >= 0 AND latitude <= 90),
     CHECK (longitude >= 0 AND longitude <= 360)*/
 );
